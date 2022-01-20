b90ce332a46075573ca19a51adb84d9cf6efdef5
Pramod Rauniyar <pramod.rauniyar@gmail.com>
2022-01-20 15:04:47 +0100

    WIP

--- index.mjs
+++ index.mjs
@@ -1,213 +1,67 @@
-import { loadStdlib } from '@reach-sh/stdlib';
+import {loadStdlib} from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
-import { ask, yesno, done } from '@reach-sh/stdlib/ask.mjs';
+const stdlib = loadStdlib(process.env);
+const thread = async (f) => await f();
+export class Signal {
+    constructor() {
+        const me = this;
+        this.Buyer = new Promise((resolve) => { me.r = resolve; });
+    }
+    wait() { return this.Buyer; }
+    notify() { this.r(true); }
+};
+
 (async () => {
-    const stdlib = await loadStdlib();
-    const startingBalance = stdlib.parseCurrency(10); // Start Balance
+    const startingBalance = stdlib.parseCurrency(100);
     const fmt = (x) => stdlib.formatCurrency(x, 4);
-    const getBalance = async () => fmt(await stdlib.balanceOf(acc));
+    const getBalance = async (acc) => fmt(await stdlib.balanceOf(acc));
+    const [ accA, accB, accC ] =
+        await stdlib.newTestAccounts(3, startingBalance);
+    accA.setDebugLabel('A');
+    accB.setDebugLabel('B');
+    accC.setDebugLabel('C');
 
-    const whoIam = await ask(
-        `Who are you? 1. Creator 2. Owner 3. Buyer 4. External user`,
-        JSON.parse
-    );
-    var who;
-    if(whoIam == 1) {
-        who = 'Creator'
-    } else if (whoIam == 2){
-        who = 'Owner'
-    } else if (whoIam == 3) {
-        who = 'Buyer'
-    }else if (whoIam == 4) {
-        who = 'External User'
-    }
-    console.log("You are "+ who);
-
-    let acc = null;
-    const createAcc = await ask(
-        `Would you like to create an account? (only possible on devnet)`,
-        yesno
-    );
-    if (createAcc) {
-        acc = await stdlib.newTestAccount(startingBalance);
-    } else {
-        const secret = await ask(
-            `What is your account secret?`,
-            (x => x)
-        );
-        acc = await stdlib.newAccountFromSecret(secret);
-    }
-
-    let ctc = null;
-    if (whoIam == 1) {
-        ctc = acc.contract(backend);
-        ctc.getInfo().then((info) => {
-            console.log(`The contract is deployed as = ${info}`); });
-    }
-
-    const before = await getBalance();
+    const ctcA = accA.contract(backend);
+    const before = await getBalance(accA);
     console.log(`Your balance is ${before}`);
-
     console.log("|-----------------------------------------------------------------------------------------------|");
-    console.log(` ${who} has ${before} Algo`);
+    console.log(` A has ${before} Algo`);
     console.log("|-----------------------------------------------------------------------------------------------|");
 
-    async function later() {
-        const after = await getBalance();
-        console.log(` ${who} went from ${before} Algo to ${after} Algo`);
-        console.log("|-----------------------------------------------------------------------------------------------|");
-    }
-    async function externalViewer() {
-        console.log(`User sees who the owner is...`);
-        const owner = await ctc.v.NFT.owner();
 
-        if(owner && owner[1]) {
-            console.log(`...it is ${stdlib.formatAddress(owner[1])}`);
-        }
-    };
-    const nftProps = {
-        nftId: stdlib.randomUInt(), //Random NFT ID
-        artistId: stdlib.randomUInt(), // Random Artist ID
-        createdAt: "Dartroom",
-        managerAddress: acc.networkAccount.addr,
+    const ctcB = accB.contract(backend, ctcA.getInfo());
+    const ctcC = accC.contract(backend, ctcA.getInfo());
+    const ready = new Signal();
+
+    const LOG = (...args) => {
+        console.log("");
+        console.log(...args);
+        console.log("");
+
     };
 
-    const makeExternalUser = async (acc,who)=>{
-        const info = await ask(
-            `Please paste the contract information:`,
-            JSON.parse
-        );
-        ctc = acc.contract(backend,  info);
-        await externalViewer();
-    }
-    const makeOwner = async (acc,ctcC, who) => {
-        let interact = {};
-        var ctc;
-        if(whoIam == 1){
-            ctc = acc.contract(backend, ctcC.getInfo());
-        } else{
-            const info = await ask(
-                `Please paste the contract information:`,
-                JSON.parse
-            );
-            ctc = acc.contract(backend,  info);
-        }
-        interact.showOwner = async (nftId, nftPrice, owner)=>{
-            console.log("|-----------------------------------------------------------------------------------------------|");
-            console.log(` (${who}) sees new ${owner}\n NFT Price: ${fmt(nftPrice)} Algo\n NFT ID: #${nftId}`);
-            console.log("|-----------------------------------------------------------------------------------------------|");
-            await later();
+    const aP = ctcA.p.Creator({
+        ready: () => {
+            LOG(`A says its ready`);
+            ready.notify();
+        },
+        log: (...args) => LOG(`A sees`, ...args),
+        checkBalance: async ()=>{
+            const after = await getBalance(accA);
+            console.log(`After your balance is ${after}`);
 
-            // if ( stdlib.addressEq(owner, acc.networkAccount.addr) ) {
-            //     console.log("|-----------------------------------------------------------------------------------------------|");
-            //     console.log(` New owner is (${who}) ${owner}\n NFT Price: ${fmt(nftPrice)} Algo\n NFT ID: #${nftId}`);
-            //     console.log("|-----------------------------------------------------------------------------------------------|");
-            //     await later();
-            // }
         }
-        interact.getAuctionProps = async ()=>{
-            const price = await ask(
-                `Enter NFT price for selling if you want to sell:`,
-                stdlib.parseCurrency
-            );
-            const timeout = 20;
-            console.log("|-----------------------------------------------------------------------------------------------|");
-            console.log(`${who} set the selling price of NFT as ${fmt(price)} Algo`);
-            console.log("|-----------------------------------------------------------------------------------------------|");
-            return {nftPrice:price,timeout:timeout};
-        }
-        return  ctc.p.Owner(interact);
-    };
-    const makeBuyer = async (acc,ctcC, who) => {
-        let interact = {};
-        var ctc;
-        console.log("I am "+whoIam);
-        if(whoIam == 1){
-            ctc = acc.contract(backend, ctcC.getInfo());
-        } else{
-            const info = await ask(
-                `Please paste the contract information:`,
-                JSON.parse
-            );
-            ctc = acc.contract(backend,  info);
-        }
-        //console.log("Contract info is: "+info);
-       // ctc = acc.contract(backend,  info);
-        interact.showOwner =async (nftId, nftPrice, owner)=>{
-            console.log("|-----------------------------------------------------------------------------------------------|");
-            console.log(` (${who}) sees new owner is ${owner}\n NFT Price: ${fmt(nftPrice)} Algo\n NFT ID: #${nftId}`);
-            console.log("|-----------------------------------------------------------------------------------------------|");
-            await later();
-            // if ( stdlib.addressEq(owner, acc.networkAccount.addr) ) {
-            //     console.log("|-----------------------------------------------------------------------------------------------|");
-            //     console.log(` New owner is (${who}) ${owner}\n NFT Price: ${fmt(nftPrice)} Algo\n NFT ID: #${nftId}`);
-            //     console.log("|-----------------------------------------------------------------------------------------------|");
-            //     await later();
-            // }else{
-            //     console.log("|-----------------------------------------------------------------------------------------------|");
-            //     console.log(` Not same New owner is (${who}) ${owner}\n NFT Price: ${fmt(nftPrice)} Algo\n NFT ID: #${nftId}`);
-            //     console.log("|-----------------------------------------------------------------------------------------------|");
-            //     await later();
-            // }
-        }
-        interact.buyNft=async (nftPrice, nftId, artistId) => {
-            console.log(` NFT ID: #${nftId}\n Artist ID: #${artistId}\n NFT Price: ${fmt(nftPrice)} Algo`);
-            const buy = await ask(
-                `Want to buy?`,
-                yesno
-            );
-            return buy;
-        }
-        interact.informTimeout = async (nftId, nftPrice, owner) => {
-            console.log(`Timeout!.`);
-            await later();
-            // process.exit(0);
-        }
-        interact.noBuy = async (buy)=>{
-            console.log(`buy=>`,buy);
-            await later();
-        }
-        return  ctc.p.Buyer(interact);
-    };
-    let interact = {};
-    if(whoIam == 1) {
+    });
 
-        interact.getNftProps = () => {
-            console.log(` Creator makes id #${nftProps.nftId}`);
-            console.log(` Artist id #${nftProps.artistId}`);
-            console.log(` CreatedAt: ${nftProps.createdAt}`);
-            console.log(` Manager Address: ${nftProps.managerAddress}`);
-            return nftProps;
-        }
-        interact.showOwner = async (nftId, nftPrice, owner) => {
-            console.log("|-----------------------------------------------------------------------------------------------|");
-            console.log(`(${who}) sees new owner is ${owner}\n NFT Price: ${fmt(nftPrice)} Algo\n NFT ID: #${nftId}`);
-            console.log("|-----------------------------------------------------------------------------------------------|");
-            await later();
-                // if ( stdlib.addressEq(owner, acc.networkAccount.addr) ) {
-                //     console.log("|-----------------------------------------------------------------------------------------------|");
-                //     console.log(` New owner is (${who}) ${owner}\n NFT Price: ${fmt(nftPrice)} Algo\n NFT ID: #${nftId}`);
-                //     console.log("|-----------------------------------------------------------------------------------------------|");
-                //     await later();
-                // }
-        }
-        interact.informTimeout = async (nftId, nftPrice, owner) => {
-            console.log(`Timeout!.`);
-            await later();
-           // process.exit(0);
-        }
-    }
-    var pr = [];
-    if(whoIam == 1 || whoIam == 2) {
-        pr.push(ctc.p.Creator(interact));
-        pr.push(makeOwner(acc,ctc,'Creator'));
+    await ready.wait();
 
-    }else if(whoIam == 3){
-        pr.push(makeBuyer(acc,ctc,'Buyer'));
-    }else if (whoIam == 4){
-       await makeExternalUser(acc,'External User');
-    }
-    await Promise.all(pr);
-    console.log("Done!");
+    await ctcB.a.Buyer.put(stdlib.parseCurrency(10));
+    await ctcC.a.Buyer.put(stdlib.parseCurrency(5));
+    await ctcA.a.Buyer.done();
+
+
+    await aP;
+
+
 
 })();
--- index.rsh
+++ index.rsh
@@ -1,90 +1,41 @@
 'reach 0.1';
-"use strict";
+'use strict';
 
-const NftProps = Object({
-    nftId: UInt,
-    artistId: UInt,
-    createdAt: Bytes(50), // Number of bytes that createdAt can take
-    managerAddress: Address,
-});
-
-const AuctionProps = Object({
-    nftPrice: UInt,
-    timeout: UInt,
-});
-
-const BidderProps = {
-    buyNft: Fun([UInt, UInt, UInt], Bool),
-};
-
-const OwnerInterface = {
-    showOwner: Fun([UInt, UInt, Address], Null),
-    informTimeout: Fun([], Null),
-    getAuctionProps: Fun([], AuctionProps)
-};
-const BuyerInterface = {
-    ...BidderProps,
-    showOwner: Fun([UInt, UInt, Address], Null),
-    informTimeout: Fun([], Null),
-    noBuy: Fun([Bool], Null),
-}
-const CreatorInterface = {
-    ...OwnerInterface,
-    getNftProps: Fun([], NftProps),
-};
-
-const emptyAuction = {
-    nftPrice: 0, timeout: 0
-};
-
-export const main = Reach.App(
-    {},
-    [
-        Participant('Creator', CreatorInterface),
-        ParticipantClass('Owner', OwnerInterface),
-        ParticipantClass('Buyer', BuyerInterface),
-        View('NFT', {owner: Address })
-    ],
-    (Creator, Owner,Buyer,vNFT) => {
-
-        Creator.only(() => {
-            const { nftId, artistId, createdAt, managerAddress } = declassify(interact.getNftProps());
-        });
-        Creator.publish(nftId, artistId, createdAt, managerAddress);
-        Owner.only(() => {
-            const amOwner = true;
-            const { nftPrice, timeout } =declassify(interact.getAuctionProps());
-        });
-        commit();
-        Owner.publish(nftPrice, timeout).when(amOwner).timeout(false);
-        
-        var owner = Creator;
-        { vNFT.owner.set(owner); };
-        invariant(balance() == 0);
-        while (true) {
-            commit();
-            Buyer.only(() => {
-                const buy = declassify(interact.buyNft(nftPrice, nftId, artistId)); // return true or false
-                const buyNft = buy && this !== owner;
-                const bidder = this;
-            })
-            Buyer.publish(buy,bidder);
-            commit();
-            Buyer.publish(buyNft).pay(nftPrice).when(buyNft).timeout(relativeTime(timeout), () => {
-                each([Creator, Owner,Buyer], () => {
-                    interact.informTimeout();
-                })
-                Anybody.publish();
-                [owner] = [owner];
-                continue;
-            });
-            each([Creator,Owner,Buyer], () => {
-                interact.showOwner(nftId, nftPrice, bidder);
-            });
-            transfer(balance()).to(owner);
-            [owner] = [owner];
-            continue;
-        };
-        commit();
-
+export const main = Reach.App(() => {
+    const Creator = Participant('Creator', {
+        ready: Fun([], Null),
+        log: Fun(true, Null),
+        checkBalance:Fun([], Null)
     });
+    const Buyer = API('Buyer', {
+        put: Fun([UInt], UInt),
+        done: Fun([], Null),
+    });
+    init();
+
+    Creator.publish();
+    //const M = new Map(UInt);
+    Creator.interact.ready();
+
+    const [done, amt] = parallelReduce([false, 0])
+        .invariant(balance() == 0)
+        .while(true)
+        .api(Buyer.done, () => {
+            assume(this == Creator);
+        }, () => 0, (k) => {
+            require(this == Creator);
+            k(null);
+            Creator.interact.log('Done!');
+            return [ true, amt ];
+        })
+        .api(Buyer.put, (x) => x, (x, k) => {
+            k(x);
+            Creator.interact.log(this, 'put', x);
+            Creator.interact.checkBalance();
+            transfer(x).to(Creator);
+            return [ done,x ];
+        })
+
+    commit();
+    exit();
+});
